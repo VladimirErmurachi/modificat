988e59c2afb350bd4f2394c08ae4296987d3fd18
Ermurachi Vladimir <ermurachi.vladimir@mail.ru>
2024-06-27 18:06:13 +0300

    todolist

---
+++ src/components/TodoWrapper.js
@@ -0,0 +1,30 @@
+import React,{useState} from 'react'
+import { TodoFrom } from './TodoForm'
+import {v4 as uuidv4} from 'uuid';
+import { Todo } from './Todo';
+uuidv4();
+
+export const TodoWrapper = () => {
+    const [todos, setTodos] = useState([])
+
+    const addTodo = todo => {
+        setTodos([...todos, {id: uuidv4(), task: todo, completed: false, isEditing: false}])
+        console.log(todos)
+        }
+
+        const toggleComplete = id => {
+            setTodos(todos.map(todo => todo.id === id ? {... todo, completed: !todo.completed} : todo))
+
+        }
+        const deleteTodo = id => {
+            setTodos(todos.filter(todo => todo.id !==id))
+    }
+    return (
+            <div className='TodoWrapper'>
+                <TodoFrom addTodo={addTodo} />
+                {todos.map((todo, index) => (
+                    <Todo task={todo} key={index} toggleComplete={toggleComplete} deleteTodo={deleteTodo}/>
+                ))}
+            </div>
+    )
+}
